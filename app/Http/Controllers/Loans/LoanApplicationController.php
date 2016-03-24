b0cef9316dd4378fec30c6a23daf6fff83da2914
Christian Jay Bayno <christianjaybayno@gmail.com>
2016-03-24 14:06:41 +0800

    on ajax request middleware for loan application

--- app/Http/Controllers/Loans/LoanApplicationController.php
+++ app/Http/Controllers/Loans/LoanApplicationController.php
@@ -39,9 +39,9 @@ class LoanApplicationController extends Controller
 	{
 		$this->loanRepo = $LoanRepository;
 		
-		// $this->middleware('ajax.request', ['except' => [
-            // 'getForm',
-        // ]]);
+		$this->middleware('ajax.request', ['except' => [
+            'getForm',
+        ]]);
 	}
 	
 	/**
