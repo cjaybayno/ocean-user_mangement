8cf16f377575e2450a427e5b5bd12483fbd096d3
Christian Jay Bayno <christianjaybayno@gmail.com>
2016-05-22 17:59:17 +0800

    fix conso queries

--- app/LoanApplication.php
+++ app/LoanApplication.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoanApplication extends Model
 {
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
-    public $timestamps = false;
+   
 }
