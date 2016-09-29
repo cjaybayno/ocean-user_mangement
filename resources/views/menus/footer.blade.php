b960a3b26e289ca4d27f98cb79ec3f2c35b71a48
Christian Jay Bayno <christianjaybayno@gmail.com>
2016-09-30 04:05:30 +0800

    update footer

--- resources/views/menus/footer.blade.php
+++ resources/views/menus/footer.blade.php
@@ -1,7 +1,7 @@
  <footer>
 	<div class="">
-		<p class="pull-right">Ocean Loan! a Loan system by <a>JingLab</a>. |
-			<span class="lead"> <i class="fa fa-anchor"></i> Ocean Loan!</span>
+		<p class="pull-right">COPYRIGHT 2015-2016 ICORE, ALL RIGHTS RESERVED
+			<!-- <span class="lead"> <i class="fa fa-anchor"></i> Ocean Loan!</span> -->
 		</p>
 	</div>
 	<div class="clearfix"></div>
