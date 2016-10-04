2ca71affc5a58d3cca9e450124a361f1e933945d
Cjay Bayno [napinstance] <christianjaybayno@gmail.com>
2016-10-04 17:38:02 +0000

    fix footer

--- resources/views/menus/footer.blade.php
+++ resources/views/menus/footer.blade.php
@@ -1,8 +1,8 @@
- <footer>
+ <center>
 	<div class="">
-		<p class="pull-right">COPYRIGHT 2015-2016 ICORE, ALL RIGHTS RESERVED
+		<p class="">COPYRIGHT 2015-2016 ICORE, ALL RIGHTS RESERVED
 			<!-- <span class="lead"> <i class="fa fa-anchor"></i> Ocean Loan!</span> -->
 		</p>
 	</div>
 	<div class="clearfix"></div>
-</footer>
+</center>
